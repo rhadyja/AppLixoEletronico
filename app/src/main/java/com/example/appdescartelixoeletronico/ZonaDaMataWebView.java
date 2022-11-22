787532b70a7650bfff61ab469a7c2006c2cf9437
wesleyadonay <adonay3005>
2022-11-21 23:35:00 -0300

    JavaScripts

--- app/src/main/java/com/example/appdescartelixoeletronico/ZonaDaMataWebView.java
+++ app/src/main/java/com/example/appdescartelixoeletronico/ZonaDaMataWebView.java
@@ -16,7 +16,7 @@ public class ZonaDaMataWebView extends AppCompatActivity {
         WebView webview = findViewById(R.id.webViewZonaDaMata);
 
         webview.setWebViewClient(new WebViewClient());
-
+        webview.getSettings().setJavaScriptEnabled(true);
         webview.loadUrl("https://www.google.com/maps/dir//Descarte+Eletr%C3%B4nicos+Caruaru+-+R.+17+-+Kennedy,+Caruaru+-+PE,+55036-720/@-8.2897645,-35.9746215,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x7a98bea1f58cec7:0xfcc9947fbeb599e3!2m2!1d-35.9724328!2d-8.2897645!3e0https://www.google.com/maps/dir//Descarte+Eletr%C3%B4nicos+Caruaru+-+R.+17+-+Kennedy,+Caruaru+-+PE,+55036-720/@-8.2897645,-35.9746215,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x7a98bea1f58cec7:0xfcc9947fbeb599e3!2m2!1d-35.9724328!2d-8.2897645!3e0");
 
     }
