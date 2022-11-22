a87b46903f3fab49b19cc1f2ddb506de6b2f8550
Rhadyja Bezerra <rgob@ecomp.poli.br>
2022-11-21 22:44:58 -0300

    criando activitys do webview

---
+++ app/src/main/java/com/example/appdescartelixoeletronico/RegiaoMetropolitanaWebView.java
@@ -0,0 +1,14 @@
+package com.example.appdescartelixoeletronico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegiaoMetropolitanaWebView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regiao_metropolitana_web_view);
+    }
+}
---
+++ app/src/main/java/com/example/appdescartelixoeletronico/ZonaDaMataWebView.java
@@ -0,0 +1,14 @@
+package com.example.appdescartelixoeletronico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ZonaDaMataWebView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zona_da_mata_web_view);
+    }
+}
