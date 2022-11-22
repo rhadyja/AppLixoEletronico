68c828d1d67ddab44d8b59863cdf3b503f27a227
Rhadyja Bezerra <rgob@ecomp.poli.br>
2022-11-22 00:01:32 -0300

    add link webview

--- app/src/main/java/com/example/appdescartelixoeletronico/AgresteWebView.java
+++ app/src/main/java/com/example/appdescartelixoeletronico/AgresteWebView.java
@@ -8,16 +8,27 @@ import android.webkit.WebViewClient;
 
 public class AgresteWebView extends AppCompatActivity {
 
+    private String link;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agreste_web_view);
 
-        WebView webview = findViewById(R.id.webViewAgreste);
+        webView(getLink());
+    }
 
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void webView(String link){
+        WebView webview = findViewById(R.id.webViewAgreste);
         webview.setWebViewClient(new WebViewClient());
         webview.getSettings().setJavaScriptEnabled(true);
-        webview.loadUrl("https://www.google.com/maps/dir//Descarte+Eletr%C3%B4nicos+Caruaru+-+R.+17+-+Kennedy,+Caruaru+-+PE,+55036-720/@-8.2897645,-35.9746215,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x7a98bea1f58cec7:0xfcc9947fbeb599e3!2m2!1d-35.9724328!2d-8.2897645!3e0https://www.google.com/maps/dir//Descarte+Eletr%C3%B4nicos+Caruaru+-+R.+17+-+Kennedy,+Caruaru+-+PE,+55036-720/@-8.2897645,-35.9746215,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x7a98bea1f58cec7:0xfcc9947fbeb599e3!2m2!1d-35.9724328!2d-8.2897645!3e0");
-
+        webview.loadUrl(link);
     }
 }
