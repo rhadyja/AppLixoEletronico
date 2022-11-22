808d1d92e6c1dc278d748506611cecd6e8ea54ee
Rhadyja Bezerra <rgob@ecomp.poli.br>
2022-11-21 22:31:22 -0300

    criando activitys

---
+++ app/src/main/java/com/example/appdescartelixoeletronico/RegiaoMetropolitana.java
@@ -0,0 +1,14 @@
+package com.example.appdescartelixoeletronico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegiaoMetropolitana extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regiao_metropolitana);
+    }
+}
