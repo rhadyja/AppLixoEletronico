d1c987f0c3a20e3ccb624866c11664ac0737e553
Rhadyja Bezerra <rgob@ecomp.poli.br>
2022-11-21 22:39:15 -0300

    adicionando intents

--- app/src/main/java/com/example/appdescartelixoeletronico/MainActivity.java
+++ app/src/main/java/com/example/appdescartelixoeletronico/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.appdescartelixoeletronico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonZonaDaMata = findViewById(R.id.buttonZonaDaMata);
+        Button buttonAgreste = findViewById(R.id.buttonAgreste);
+        Button buttonRMR = findViewById(R.id.buttonRMR);
+
+        buttonZonaDaMata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this, ZonaDaMata.class);
+                startActivity(intent1);
+            }
+        });
+
+        buttonAgreste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(MainActivity.this, Agreste.class);
+                startActivity(intent2);
+            }
+        });
+
+        buttonRMR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3 = new Intent(MainActivity.this, RegiaoMetropolitana.class);
+                startActivity(intent3);
+            }
+        });
     }
 }
